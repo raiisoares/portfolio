b98586e9f02714ecdf1bebc15ce84d0e86f7f205
raiisoares <raisoares.dev@gmail.com>
2024-11-09 18:02:40 -0300

    refact: handle hero animation theme change & modify theme to orange

--- src/components/animations/hero-animation.tsx
+++ src/components/animations/hero-animation.tsx
@@ -1,13 +1,22 @@
 import Lottie from 'react-lottie-player'
 import hero from '@/assets/lottie/software-development.json'
+import { useTheme } from 'next-themes'
 
 export function HeroAnimation() {
+  const { theme } = useTheme()
+
+  const lottieStyle = {
+    width: '100%',
+    height: 450,
+    filter: theme === 'dark' ? 'invert(1) brightness(0.8)' : 'none',
+  }
+
   return (
     <Lottie
       animationData={hero}
       loop
       play
-      style={{width: '100%', height: 450}}
+      style={lottieStyle}
       speed={0.8}
     />
   )
