0dc498535fe8ca177ddb7002c31575d35710640b
raiisoares <raisoares.dev@gmail.com>
2025-02-13 19:29:22 -0300

    refact: get current year on footer; style: fix tech cards alignment

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
 
         <div className={'flex items-center justify-between pt-8'}>
           <p className={'text-xs text-zinc-500 align-middle'}>
-            &copy; 2024 All rights reserved.
+            &copy; {new Date().getFullYear()} All rights reserved.
           </p>
           <NavLinks/>
         </div>
