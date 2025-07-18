424e535ddd98dce295d1817ff4b03a705ca74489
raiisoares <raisoares.dev@gmail.com>
2025-07-18 18:29:35 -0300

    refact/make-landing-page-ssg

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,9 @@
 import { HomePage } from '@/components/pages/home-page'
+import { routing } from '@/i18n/routing'
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }))
+}
 
 export default function Home() {
   return <HomePage />
