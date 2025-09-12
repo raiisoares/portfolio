c57126fd52c1aa6c26718665a135d0007f9aef6c
Raí Soares <raisoares.dev@gmail.com>
2025-09-12 11:15:46 -0300

    refact: linting code

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,18 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-        {
-            url: `${process.env.BASE_URL}/pt`,
-            lastModified: new Date(),
-            changeFrequency: 'yearly',
-            priority: 1,
-        },
-        {
-            url: `${process.env.BASE_URL}/en`,
-            lastModified: new Date(),
-            changeFrequency: 'yearly',
-            priority: 1,
-        },
-    ]
+  return [
+    {
+      url: `${process.env.BASE_URL}/pt`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${process.env.BASE_URL}/en`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+  ]
 }
