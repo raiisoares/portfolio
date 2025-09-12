f2d3cfa77136953ee0cbf268fb1c6a30c7d68082
Raí Soares <raisoares.dev@gmail.com>
2025-09-12 11:14:09 -0300

    refactor: update hero infos; feat: add robots.ts, sitemap.ts and improve SEO configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,49 @@
-import type { Metadata } from 'next'
+import type {Metadata} from 'next'
 import './globals.css'
-import { JetBrains_Mono } from 'next/font/google'
+import {JetBrains_Mono} from 'next/font/google'
 import type React from 'react'
-import { Providers } from '@/app/providers'
+import {Providers} from '@/app/providers'
 
 const jetBrainsMono = JetBrains_Mono({
-  subsets: ['latin'],
-  display: 'swap',
+    subsets: ['latin'],
+    display: 'swap',
 })
 
 export const metadata: Metadata = {
-  title: 'Raí Soares',
-  description: `Raí Soares' Portfolio`,
-  icons: {
-    icon: ['/favicon.ico'],
-  },
+    title: 'Raí Soares | Desenvolvedor Full Stack',
+    description: `Desenvolvedor Full Stack especializado em React, Next.js e Spring Boot. Transformando ideias em 
+    software eficiente e escalável.`,
+    robots: 'index, follow',
+    icons: {
+        icon: ['/favicon.ico'],
+    },
+    twitter: {
+        card: 'summary_large_image'
+    },
+    openGraph: {
+        title: 'Raí Soares',
+        description: 'Transformando ideias em software.',
+        url: 'https://raisoares.com/og-image.png',
+        siteName: 'Raí Soares',
+        locale: 'pt_BR',
+        type: 'website',
+        images: [
+            {
+                url: 'https://raisoares.com/og-image.png',
+                width: 800,
+                height: 600,
+                alt: 'Raí Soares',
+            },
+        ],
+    },
 }
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang='pt-BR' suppressHydrationWarning>
-      <body className={`${jetBrainsMono.className} antialiased`}>
+export default async function RootLayout({children}: { children: React.ReactNode }) {
+    return (
+        <html lang='pt-BR' suppressHydrationWarning>
+        <body className={`${jetBrainsMono.className} antialiased`}>
         <Providers>{children}</Providers>
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
