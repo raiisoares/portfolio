2a96a856222d03aaa89ee766741c2ac3fc02377e
Raí Soares <raisoares.dev@gmail.com>
2025-09-15 19:33:04 -0300

    refactor: update headers

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,12 @@ const nextConfig: NextConfig = {
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
         ],
       },
     ]
